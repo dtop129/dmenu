a40bd87218d9dac732e31dcd6b1f13e6817c2f80
dtop <dantop912@gmail.com>
2021-01-06 16:38:05 +0100

    Changed fonts

--- config.def.h
+++ config.def.h
@@ -4,7 +4,8 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"xos4 Terminus:size=10"
+	"JetBrainsMonoMedium Nerd Font:size=10",
+	"Noto Sans CJK JP Medium:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
