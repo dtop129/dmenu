2e250bf6c5956eafaef5601961ac58e61687342c
dtop <dantop912@gmail.com>
2021-01-28 19:23:42 +0100

    Changed colours

--- config.def.h
+++ config.def.h
@@ -12,16 +12,10 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char col_base00[]      = "#151515";
-static const char col_base01[]      = "#202020";
-static const char col_base02[]      = "#303030";
-static const char col_base04[]      = "#b0b0b0";
-static const char col_base0C[]      = "#12cfc0";
-static const char col_base0D[]      = "#6fc2ef";
 static const char *colors[SchemeLast][2] = {
 	           /*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { col_base00, col_base0D },
+	[SchemeSel]  = { "#00a8f3", "#303030" },
 	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
