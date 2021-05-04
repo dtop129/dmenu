46c9cea05f7ec578f9aff4c1b7c756577e0182db
dtop <dantop912@gmail.com>
2021-05-04 18:26:03 +0200

    Changed font size

--- config.def.h
+++ config.def.h
@@ -8,8 +8,8 @@ static int nostdin = 0;                    /* -n option, if 1, disable stdin, us
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10",
-	"JoyPixels:size=10"
+	"monospace:size=11",
+	"JoyPixels:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
